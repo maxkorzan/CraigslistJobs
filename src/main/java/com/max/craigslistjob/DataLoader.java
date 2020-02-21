f568a3a9147664012f8f39a8ca9307129aefe486
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-21 13:19:28 -0500

    second commit

--- src/main/java/com/max/craigslistjob/DataLoader.java
+++ src/main/java/com/max/craigslistjob/DataLoader.java
@@ -35,6 +35,27 @@ public class DataLoader implements CommandLineRunner {
         job = new Job("Project Manager","Run the game. Take over the world.","Feb 16, 2020","Facebook","555-555-5555", "www.facebook.com");
         repository.save(job);
 
+        job = new Job("Junior Developer","Coding on team projects","Dec 21, 2019","Oracle","555-555-5555", "www.oracle.com");
+        repository.save(job);
+
+        job = new Job("Front-End Developer","Making our sites pretty","Nov 21, 2019","Apple","555-555-5555", "www.apple.com");
+        repository.save(job);
+
+        job = new Job("Full Stack Developer","Manange all aspects of our product","Jan 15, 2020","Motivation Industries","555-555-5555", "www.motivation.com");
+        repository.save(job);
+
+        job = new Job("Project Manager","Run the game. Take over the world.","Feb 16, 2020","Facebook","555-555-5555", "www.facebook.com");
+        repository.save(job);
+
+        job = new Job("Front-End Developer","Making our sites pretty","Nov 21, 2019","Apple","555-555-5555", "www.apple.com");
+        repository.save(job);
+
+        job = new Job("Junior Developer","Coding on team projects","Dec 21, 2019","Oracle","555-555-5555", "www.oracle.com");
+        repository.save(job);
+
+        job = new Job("Project Manager","Run the game. Take over the world.","Feb 16, 2020","Facebook","555-555-5555", "www.facebook.com");
+        repository.save(job);
+
 
 
     }
